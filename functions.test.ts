3bc401144f7ac29d6cf03481ccb243788a71d9c9
Keaton Player <keatonplayer18@gmail.com>
2022-04-29 16:04:13 -0600

    Fixed test

--- functions.test.ts
+++ functions.test.ts
@@ -1,8 +1,13 @@
 const {shuffleArray} = require('./utils')
 
+let testArr = [1,2,3,4]
 describe('shuffleArray should', () => {
     test('Array is returned', function(){
-    expect(shuffleArray(Array).toHaveReturned())
+    expect(Array.isArray(shuffleArray(testArr))).toBe(true)
     })
-
+     
+it('should check array length to be the same as output', function(){
+    expect(shuffleArray(testArr)).toHaveLength(testArr.length)
 })
+})
+
