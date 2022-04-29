246fb03fbabd4a0f16ce80e73b3e6dfe7959b3cd
Keaton Player <keatonplayer18@gmail.com>
2022-04-29 10:09:19 -0600

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(path.join(__dirname, '../public')))
-
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
 
+app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/api/robots', (req, res) => {
